8e49fc745367fd0372ebc176fdc0fa374f753861
George <George.e.m.178@gmail.com>
2023-03-05 18:06:37 +0200

    bugs fixed & added features (like order by name) & Project DONE

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const fs = require("fs");
-
 const path = require("path");
 
 const express = require("express");
+
+const defaultRoutes = require("./routes/default");
+const restaurantRoutes = require("./routes/restaurants");
 
 const app = express();
 
@@ -10,51 +11,17 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.use("/", defaultRoutes);
+app.use("/", restaurantRoutes);
+
+app.use((req, res) => {
+  res.status(404).render("404");
 });
 
-app.get("/restaurants", (req, res) => {
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-
-  const fileData = fs.readFileSync(filePath);
-
-  const storedRestaurants = JSON.parse(fileData);
-  res.render("restaurants", {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
-});
-
-app.get("/recommend", (req, res) => {
-  res.render("recommend");
-});
-
-app.post("/recommend", (req, res) => {
-  const restaurant = req.body;
-
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-
-  const fileData = fs.readFileSync(filePath);
-
-  const storedRestaurants = JSON.parse(fileData);
-
-  storedRestaurants.push(restaurant);
-
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
-
-  res.redirect("/confirm");
-});
-
-app.get("/confirm", (req, res) => {
-  res.render("confirm");
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
+app.use((error, req, res, next) => {
+  res.status(500).render("500");
 });
 
 app.listen(3000);
